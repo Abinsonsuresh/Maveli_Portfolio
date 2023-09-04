35962c37f28b435090bee46c61339d3b31a256fc
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-09-05 02:49:48 +0530

    fixed

--- src/Pages/Timeline.jsx
+++ src/Pages/Timeline.jsx
@@ -4,6 +4,7 @@ import 'react-vertical-timeline-component/style.min.css';
 import { Timelines } from '../Data/TimelineData';
 import bgflr from '../assets/bgflr1.png'
 import flwr from '../assets/flwr.png'
+import { motion } from 'framer-motion'
 
 const Timeline = () => {
     return (
@@ -20,11 +21,14 @@ const Timeline = () => {
                 <div className='absolute top-[70%] left-100 opacity-60'>
                     <img src={flwr} className='w-full' alt="" />
                 </div>
+
                 <VerticalTimeline>
                     {
                         Timelines.map((data) => (
+
                             <VerticalTimelineElement
                             key={data.id}
+                                animate={ true}
                                 className="vertical-timeline-element--work"
                                 contentStyle={{ background: '#fbf7d5', color: '#000000' }}
                                 contentArrowStyle={{ borderRight: '7px solid  #fbf7d5' }}
