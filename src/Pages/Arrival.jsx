91a9ac2d78e5ce4295c42da509d412108fe8ed70
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-09-05 23:46:58 +0530

    errors fixed

--- src/Pages/Arrival.jsx
+++ src/Pages/Arrival.jsx
@@ -48,7 +48,7 @@ const Arrival = () => {
 
     return (
         <>
-            <div className='flex justify-center my-8 h-full min-h-[30vh]'>
+            <div className='flex justify-center my-8 h-full'>
                 <h3 className='font-bold text-6xl'>Next Arrival</h3>
             </div>
 
