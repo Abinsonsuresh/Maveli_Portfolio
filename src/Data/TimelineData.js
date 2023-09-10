2324959496763d1b3309276ad5a66a382e49c6a6
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-09-10 22:07:46 +0530

    bugs fixed

--- src/Data/TimelineData.js
+++ src/Data/TimelineData.js
@@ -8,12 +8,50 @@ export const Timelines = [
 
     {
         id: 2,
-        date: "1000 - present",
         title: "Ascension to the Throne",
         description: "I became the king of the Asura (demon) kingdom. Under my rule, my kingdom prospered, and I was known for my just and benevolent rule. I earned the title 'Mahabali Chakravarthi,' which means the great king."
 
         ,
     },
 
-    
+    {
+        id: 3,
+        title: "Benevolent Rule",
+        description: " During my reign, my kingdom witnessed a period of great prosperity and happiness. My subjects were content, and there was no poverty or suffering in my kingdom.",
+    },
+
+    {
+        id: 4,
+        title: "Encounter with Lord Vishnu",
+        description: "Despite my virtuous rule, my growing power and popularity began to concern the gods, especially Lord Indra, the king of the heavens. To curb my influence, Lord Vishnu took the form of a dwarf Brahmin boy named Vamana.",
+    },
+
+    {
+        id: 5,
+        title: "Vamana's Visit",
+        description: "Vamana visited my kingdom and requested three paces of land for a yajna (sacrificial ritual). Known for my generosity, I agreed without hesitation. However, Vamana, in his divine form, began to expand, covering the entire universe in three steps.",
+    },
+
+    {
+        id: 6,
+        title: "My Sacrifice",
+        description: "Realizing that Vamana was none other than Lord Vishnu, I offered my own head as the third step, showing my unwavering devotion and humility. Lord Vishnu was pleased with my devotion and granted me a boon.",
+    },
+
+    {
+        id: 7,
+        title: "Benevolent Banishment",
+        description: "As a boon, I requested permission to visit my kingdom and my people once a year. This request was granted, and I was allowed to return to Earth annually during the festival of Onam, symbolizing my annual homecoming.",
+    },
+
+    {
+        id: 8,
+        title: "Onam Festival",
+        description: "To this day, the festival of Onam is celebrated in Kerala to welcome my return. It is a time of joy, feasting, and cultural festivities. The story of Mahabali is retold through various rituals and performances during this festival.",
+    },
+    {
+        id: 9,
+        title: "Legacy",
+        description: "I am remembered as a righteous and generous king in Hindu mythology. My story serves as a reminder of the importance of humility, devotion, and righteousness. The annual celebration of Onam in Kerala continues to honor my memory.",
+    },
 ]
